44f9d775b3ef609cc698d03be3a7e12f87128c9c
Daniel Nichols <contact@danielnichols.me>
2023-01-18 18:15:10 +1100

    add lodash to project
    Also included webpack and babel plugins to reduce lodash size

--- craco.config.ts
+++ craco.config.ts
@@ -1,1 +1,11 @@
-module.exports = {};
+const LodashPlugin = require("lodash-webpack-plugin");
+module.exports = {
+  webpack: {
+    plugins: {
+      add: [new LodashPlugin()],
+    },
+  },
+  babel: {
+    plugins: ["lodash"],
+  },
+};
