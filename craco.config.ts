eb73fa966cccadc3c3ee9b15753a85cd1dc8aab7
Daniel Nichols <contact@danielnichols.me>
2023-01-18 18:44:20 +1100

    use Preact in build
    Preact is way smaller, so any usage of react will be changed to Preact in build

--- craco.config.ts
+++ craco.config.ts
@@ -1,6 +1,12 @@
 const LodashPlugin = require("lodash-webpack-plugin");
 module.exports = {
   webpack: {
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat", // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
     plugins: {
       add: [new LodashPlugin()],
     },
